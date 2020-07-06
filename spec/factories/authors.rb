a0398c7e5a27d7c4c660127f0a53886d40d316af
Idan <idan.zonshein@powerhrg.com>
2020-07-06 08:20:55 -0400

    Done.

---
+++ spec/factories/authors.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :author do
+    name "Rick"
+    phone_number "1234567890"
+  end
+end
---
+++ spec/factories/posts.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :post do
+    title "Won't Believe"
+    content "Idan"*200
+    summary "13123123123"
+    category "Fiction"
+  end
+end
