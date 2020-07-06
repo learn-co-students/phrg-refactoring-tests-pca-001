644b02ddc74c87c8cc3a2d49930bf728962cb96e
Stephen McKeon <stephen.mckeon@powerhrg.com>
2020-07-06 08:14:58 -0400

    Done.

---
+++ spec/factories/authors.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :author do
+    name "Steve"
+    phone_number 8564045086
+  end
+end
---
+++ spec/factories/posts.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :post do
+    title "Top 1 Post"
+    content "Lots of content Lots of content Lots of content Lots of content Lots of content Lots of content Lots of content Lots of content Lots of content Lots of content Lots of content Lots of content Lots of content Lots of content Lots of content Lots of content Lots of content Lots of content Lots of content Lots of content Lots of content Lots of content Lots of content Lots of content Lots of content Lots of content Lots of content"
+    summary "Not so much content"
+    category "Fiction"
+  end
+end
