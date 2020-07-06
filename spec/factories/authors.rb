c1f6cda178ca39a56b2f9fd01a9bc301e711a4a5
Aaron Wilson <adub65@gmail.com>
2020-07-06 08:47:36 -0400

    refactored post abd author specs, added factories

---
+++ spec/factories/authors.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :author do
+    name "Aaron Wilson"
+    phone_number 3048675309
+  end
+end
---
+++ spec/factories/posts.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :post do
+    title "Won't Believe Power of Difference"
+    content "A powerful and inspiring examination of the connection between the potential for great talent and conditions commonly thought to be “disabilities,” revealing how the source of our struggles can be the origin of our greatest strengths. In The Power of Different, psychiatrist and bestselling author Gail Saltz examines the latest scientific discoveries, profiles famous geniuses who have been diagnosed with all manner of brain “problems”—including learning disabilities, ADD, anxiety, Depression, Bipolar disorder, schizophrenia, and Autism—and tells the stories of lay individuals to demonstrate how specific deficits in certain areas of the brain are directly associated with the potential for great talent."
+    summary "Be the best you"
+    category "Non-Fiction"
+  end
+end
